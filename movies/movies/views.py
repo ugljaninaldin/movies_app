37fe21084a9245750640cfad72e60479c4ac1bf4
Aldin Ugljanin <aldinugljaninn@gmail.com>
2023-11-13 14:44:03 +0100

    Adding and deleting movies

--- movies/movies/views.py
+++ movies/movies/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .models import Movie
 
@@ -12,3 +12,18 @@ def home(request):
 def detail(request, id):
     data = Movie.objects.get(pk=id)
     return render(request, 'movies/detail.html', {'movie': data})
+
+def add(request):
+    title = request.POST.get("title")
+    year = request.POST.get("year")
+
+    if title and year:
+        movie = Movie(title=title, year=year)
+        movie.save()
+        return HttpResponseRedirect('/movies')
+
+    return render(request, "movies/add.html")
+
+def delete(request, id):
+    Movie.objects.get(pk=id).delete()
+    return HttpResponseRedirect("/movies")
